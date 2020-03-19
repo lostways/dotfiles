7aaade97bf0055c09da20869a80f0346acb2fe7b
lostways <andrew@lostways.com>
2020-03-19 01:25:24 -0700

    added install-deps

---
+++ install-deps.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/zsh
+
+# Installs dependancies for dotfiles
+
+
+# CURL
+sudo apt-get -y install curl
+
+# VIM
+sudo apt-get -y install vim
+
+# Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Tmux
+sudo apt-get -y install tmux
+
+# TPM
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# ZSH
+sudo apt-get -y install zsh
+
+# Oh-My-ZSh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Set ZSH to default shell
+sudo chsh -s /usr/bin/zsh
