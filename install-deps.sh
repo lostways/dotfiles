ea1843b90fc14357987b67d7b8479f423edd3d2f
Andrew Lowe <pcmaho@gmail.com>
2020-03-26 14:44:34 -0700

    removed sudo from chsh in install-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -38,4 +38,4 @@ sudo apt-get -y install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Set ZSH to default shell
-sudo chsh -s /usr/bin/zsh ${USER}
+chsh -s /usr/bin/zsh ${USER}
