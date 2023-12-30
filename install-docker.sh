58d6949500558bd37b44e7a1d99a16df1f0df489
lostways <andrew@lostways.com>
2023-12-29 20:16:37 -0800

    added docker install

---
+++ install-docker.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Add Docker's official GPG key
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+## Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+cat /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+
+# Install Docker Engine
+sudo apt-get install -y \
+  docker-ce \
+  docker-ce-cli \
+  containerd.io \
+  docker-buildx-plugin \
+  docker-compose-plugin
+
+# Post Install
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+# Verify Installation
+docker run hello-world
