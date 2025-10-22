8673e36d67c5646c0edece41679a35e2ed858e17
lostways <andrew@lostways.com>
2025-10-22 00:51:34 -0700

    wlogout

--- scripts/hyprland.sh
+++ scripts/hyprland.sh
@@ -6,7 +6,8 @@ sudo apt install -y hyprland \
   hyprpaper \
   swayosd \
   dunst \
-  wl-clipboard
+  wl-clipboard \
+  wlogout
 
 sudo apt install -y build-essential cmake git meson ninja-build \
     libwayland-dev wayland-protocols libcairo2-dev libjpeg-dev \
